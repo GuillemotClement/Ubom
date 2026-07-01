b115165bccea9b9a8c64e0a04e3e9050d47deed0
GuillemotClement <guillemot.clement@protonmail.com>
2026-07-01 09:37:33 +0200

    feat: add register form

--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Login } from '../../components/auth/login/login';
 import { Register } from '../../components/auth/register/register';
+import { AuthService } from '../../services/core/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +10,21 @@ import { Register } from '../../components/auth/register/register';
 })
 export class Home {
   // création d'un signal
-  // sa valeur initial est true 
+  // sa valeur initial est true
   // correspond à une variable réactive
   // isLogin() => permet de lire la valeur
   isLogin = signal(true);
 
   // déclaration d'une méthode appelée lorsque l'on veux changer d'écran
-  toggleAuthMode(){
+  toggleAuthMode() {
     // permet de toggle la valeur de la variable
-    this.isLogin.update(value => !value);
+    this.isLogin.update((value) => !value);
   }
 
   // méthode qui reçoit la valeur envoyée par un composant enfant (login ou register)
-  setAuthMode(isLogin: boolean){
+  setAuthMode(isLogin: boolean) {
     this.isLogin.set(isLogin);
   }
+
+  protected authService = inject(AuthService);
 }
